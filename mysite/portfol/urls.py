85e672487759f19d327d002648193fcb80cee1cf
KiranVK <kiran.kulkarni1998@gmail.com>
2021-02-02 05:31:58 +0000

    Making a portfolio website hence created a new portfol app

---
+++ mysite/portfol/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, reverse_lazy
+from .views import PortListView, PortProjectView, PortContactView, PortSkillsView, PortEducationView
+
+
+app_name='port'
+
+urlpatterns=[
+    path('', PortListView, name='about_me'),
+    path('project', PortProjectView, name='projects'),
+    path('contact', PortContactView, name='contact'),
+    path('skills', PortSkillsView, name='skills'),
+    path('education', PortEducationView, name='education'),
+]
---
+++ mysite/portfol/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+# Create your views here.
+
+
+def PortListView(request):
+    return render(request,"portfol/about.html")
+
+
+def PortProjectView(request):
+    return render(request,'portfol/project.html')
+
+def PortContactView(request):
+    return render(request,'portfol/contact.html')
+
+def PortSkillsView(request):
+    return render(request,'portfol/skills.html')
+
+def PortEducationView(request):
+    return render(request,'portfol/education.html')
